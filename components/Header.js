332d95bfee23c51f2ebd8b14a5a96623b1bf00af
peter safwat <petersafwat640@gmail.com>
2022-08-09 21:03:47 +0200

    added firebase, nextauth and intialized firebase

--- components/Header.js
+++ components/Header.js
@@ -7,14 +7,15 @@ import myImag from '../public/11111.jpg'
 
 const Header = () => {
   return (
-    <div className='border-b-1 sticky left-0 top-0 z-30 bg-white border-gray-300 pt-2 pb-4 shadow-lg shadow-gray'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto  '>
+      
+    <div className='border-b-1 sticky top-0 z-30 bg-white border-gray-300 pt-2 pb-4 shadow-md shadow-gray'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto mt-2 '>
                     {/*left */}
-            <div className='w-24 h-24 relative hidden lg:grid'>
+            <div className='w-24 h-14 relative hidden lg:grid'>
                 <Image src='https://logo-logos.com/2016/10/Instagram_logo_wordmark_logotype.png' alt='instagram' layout='fill'
                 className='object-contain ' />
             </div>
-            <div className='w-24 h-24 relative lg:hidden grid'>
+            <div className='w-24 h-14 relative lg:hidden grid'>
                 <Image src='https://static01.nyt.com/images/2016/05/11/us/12xp-instagram/12xp-instagram-videoSixteenByNineJumbo1600-v2.jpg' alt='instagram' layout='fill'
                 className='object-contain ' />
             </div>
@@ -24,9 +25,9 @@ const Header = () => {
                 <FiSearch className='absolute left-2 top-[11px] h-5 text-gray-500 cursor-pointer'/>
             </div>
             {/*right */}
-            <div className='flex items-center justify-center '>
-                <HiHome className=' hover:scale-110 ease-in duration-100 mx-4 cursor-pointer' size={35}/>
-                <BsPlusCircle className='hover:scale-110 ease-in duration-100 mx-4 cursor-pointer' size={35}/>
+            <div className='flex items-center justify-center pr-2'>
+                <HiHome className=' hover:scale-110 ease-in duration-100 mx-4 cursor-pointer' size={25}/>
+                <BsPlusCircle className='hover:scale-110 ease-in duration-100 mx-4 cursor-pointer' size={25}/>
                 <div className='mx-4 hover:scale-110 ease-in duration-100'>
                     <Image className=' rounded-full cursor-pointer ' src={myImag} alt='/' width='60px' height='60px'/>                    
                 </div>
---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp,getApp,getApps } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+import {getStorage} from 'firebase/storage'
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIRBASE_API_KEY,
+  authDomain: process.env.FIRBASE_AUTH_DOMAIN,
+  projectId:process.env.FIREBASE_PROJECT_ID ,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGENG_SENDER_ID ,
+  appId:process.env.FIREBASE_API_ID ,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = !getApps().length()? initializeApp(firebaseConfig): getApp();
+const analytics = getAnalytics(app);
+const db = getFirestore();
+const storage = getStorage();
+export {app, storage, db} 
